3bd469a1ff9f9cdea1347e3460d7baa3ce4caf14
zhangshichn <iamzs@aliyun.com>
2018-06-29 17:28:27 +0800

    add args_function.py

---
+++ 2018/args_function.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+#Filename: args_function.py
+
+def product(*num):
+    result = 1
+    if num:
+        for n in num:
+            result = result * n
+        return result
+    else:
+        raise TypeError('Null')
+
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+
+if product(5) != 5:
+    print('测试失败!')
+elif product(5, 6) != 30:
+    print('测试失败!')
+elif product(5, 6, 7) != 210:
+    print('測試失敗!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
